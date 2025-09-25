4b7d2be8d0b31a56a1342d8cc1312b174e03ba74
Thomas Garcia <144739053+epicsharkbait8@users.noreply.github.com>
2025-09-25 18:02:54 +0000

    mostly done, just need to fix the semicolons in the patrol function

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ setGravity(800);
 // For Day 1, we only need the player's sprite.
 loadSprite("apple", "https://kaboomjs.com/sprites/apple.png");
 
+loadSprite("enemy", "https://kaboomjs.com/sprites/apple.png");
 
 // --- Main Game Scene ---
 scene("main", () => {
@@ -53,6 +54,34 @@ scene("main", () => {
         "player",
     ]);
 
+    function patrol(){
+        return{
+            id:"patrol",
+            require: ["pos","area"],
+            dir: -1,
+            update(){
+                this.move(60 * this.dir, 0)
+            },
+            add(){
+                this.onCollide(obj, ocl) =>{
+                    if (col.isleft() || col.isright()){
+                        this.dir = -this.dir;
+                    }
+                 });
+                },
+            };
+        }
+
+
+    //adding enemy to scene
+    const enemy = add([
+        sprite("enemy"),
+        pos(600, 200), //starting pos for enemy
+        area(),
+        body(),
+        patrol(),//use the patrol function we just defined
+        "enemy"
+    ]);
     // --- Player Controls & Interactions ---
     onKeyDown("left", () => {
         player.move(-200, 0);
@@ -67,8 +96,27 @@ scene("main", () => {
             player.jump(650);
         }
     });
+    //colision detection
+player.onCollide("enemy",(enemy,col)=>{
+    if (col.isBottom){
+        destroy(enemy);
+        player.jump(300);
+    }
+    else{
+        destroy(player);
+        go("lose")
+    }
+});
 });
 
+//game over scene
+scne("lose", () =>{
+    add([
+        text("Game Over"),
+        post(center()),
+        anchor("center"),
+    ]);
+});
 // Start the game
 go("main");
 
